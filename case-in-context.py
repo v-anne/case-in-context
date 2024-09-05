7f430dee5bc56d480a6951d6c01b338af5eda239
v_anne <69829523+v-anne@users.noreply.github.com>
2024-09-05 10:24:19 -0400

    getting search results from courtlistener

--- case-in-context.py
+++ case-in-context.py
@@ -5,6 +5,7 @@ from openai import OpenAI
 
 BEARER_TOKEN = os.getenv("BEARER_TOKEN")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+COURTLISTENER_API_KEY = os.getenv("COURTLISTENER_API_KEY")
 
 
 twitter_url = "https://api.twitter.com/2/tweets/search/recent"
@@ -18,6 +19,12 @@ twitter_headers = {
 
 response = requests.get(twitter_url, headers=twitter_headers, params=twitter_query_params)
 
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+else:
+    print(f"Failed to fetch data: {response.status_code}")
+
 # LLM function call
 client = OpenAI()
 
@@ -26,7 +33,7 @@ response = client.chat.completions.create(
   messages=[
     {
       "role": "system",
-      "content": "You will be provided with a news article, and then have to extract certain information in JSON format. Return a JSON with the following structure: {Judge: "", Date: "", Court: ""}"
+      "content": "You will be provided with a news article, and then have to extract certain information in JSON format. Return a JSON with the following structure: {Judge: "", Date: "", Court: ""}. Court should be abbreviated using this courtlistener page: https://www.courtlistener.com/help/api/jurisdictions/ e.g., ca5, flmd, etc."
     },
     {
       "role": "user",
@@ -40,3 +47,22 @@ response = client.chat.completions.create(
 
 result = response.choices[0].message
 
+courtlistener_url = "https://www.courtlistener.com/q"
+courtlistener_query_params = {
+    'q': "",
+    'court_id': result["Court"],
+    'assigned_to_id': result["Judge"],
+    'type': 'r'
+}
+
+courtlistener_headers = {
+    'Authorization': f'Token {COURTLISTENER_API_KEY}'
+}
+
+response = requests.get(courtlistener_url, headers=courtlistener_headers, params=courtlistener_query_params)
+
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+else:
+    print(f"Failed to fetch data: {response.status_code}")
